6c54ba32b7ac28ab3b3ac7323d2a55582b189530
everything-in-sync <robkososki@me.com>
2021-12-22 02:07:48 -0500

    formatting

--- asheville.py
+++ asheville.py
@@ -1,14 +1,11 @@
-print ('Follow the prompts to check or receive weather information')
-checkWeather = input ('Would you like to check the weather? (y/n)')
-if checkWeather == 'n':
-    city = input ('Which city would you like to check the weather for?')
-    print (city)
-elif checkWeather == 'y':
-    email = input ('What email address would you like to receive weather notifications?')
-    confirmEmail = input ('You would like to receive weather notifications at') + email + (' . /n')
-    confrimEmailNotif = input ('Is that correct? (y/n)')
-    
-    
+print ('Follow the prompts to check or receive weather information ')
+checkWeather = input ('Would you like to check the weather? (y/n) ')
 
-# email = input('What is your email address?')
-# print('You entered: ' + email)
+
+if checkWeather == 'n':
+    city = input ('Which city would you like to check the weather for? ')
+
+
+elif checkWeather == 'y':
+    email = input ('Which email address would you like to receive weather notifications? ')
+    confirmEmail = input ('Your email address is: ' + email + 'is that correct? (y/n? ' )
